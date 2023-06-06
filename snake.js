85d0d5761f415daefcb2c28b83734f94c8abd8d9
Michael Houmann <mhoumann87@gmail.com>
2023-06-06 11:22:22 +0200

    Change to javascript class

--- snake.js
+++ snake.js
@@ -1,16 +1,19 @@
-function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xSpeed = 0;
-  this.ySpeed = 0;
+class Snake {
+  constructor(_x, _y, _size) {
+    this.x = _x;
+    this.y = _y;
+    this.size = _size;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+  }
 
-  this.update = function () {
+  update() {
     this.x += this.xSpeed;
     this.y += this.ySpeed;
-  };
+  }
 
-  this.show = function () {
+  show() {
     fill(255);
-    rect(this.x, this.y, 10, 10);
-  };
+    rect(this.x, this.y, this.size, this.size);
+  }
 }
