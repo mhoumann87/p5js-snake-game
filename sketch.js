8728d5c7659650296d44e9bb8c01d0373adf45e7
Michael Houmann <mhoumann87@gmail.com>
2023-06-06 11:23:36 +0200

    Change to variables for x, y and size

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 let s;
+let posX = 0;
+let posY = 0;
+const size = 10;
 
 function setup() {
   const canvas = createCanvas(600, 600);
   canvas.parent('canvas');
 
-  s = new Snake();
+  s = new Snake(posX, posY, size);
 }
 
 function draw() {
