b50aa128cbec58e7a70101bf957a6db5c86c5344
Michael Houmann <mhoumann87@gmail.com>
2023-09-13 11:05:38 +0200

    Update class, sketch and css

--- sketch.js
+++ sketch.js
@@ -1,18 +1,37 @@
-let s;
+// Variable for the snake, starting position and size
+let snake;
 let posX = 0;
 let posY = 0;
 const size = 10;
 
 function setup() {
+  // Create the canvas and put it in a div on the page
   const canvas = createCanvas(600, 600);
   canvas.parent('canvas');
 
-  s = new Snake(posX, posY, size);
+  // Create a new snake
+  snake = new Snake(posX, posY, size);
 }
 
 function draw() {
-  background(0);
+  background(51);
 
-  s.update();
-  s.show();
+  // Use the keyPressed function to move the snake
+  keyPressed();
+  snake.update();
+  snake.show();
+}
+
+// Use the arrow keys to change the direction of the snake
+function keyPressed() {
+  // Use `keyCodes` to set direction
+  if (keyCode === UP_ARROW) {
+    snake.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    snake.dir(-1, 0);
+  }
 }
--- snake.js
+++ snake.js
@@ -1,19 +1,32 @@
+// Use a class for the snake
+
 class Snake {
+  // Create and initializes the snake
   constructor(_x, _y, _size) {
+    // Place the starting point for the snake
     this.x = _x;
     this.y = _y;
+    // Set the size
     this.size = _size;
+    // Set the speed for the snake
     this.xSpeed = 0;
     this.ySpeed = 0;
   }
 
+  // Move the snake
   update() {
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
+    this.x = this.xSpeed;
+    this.y = this.ySpeed;
   }
 
+  // Display the snake
   show() {
     fill(255);
     rect(this.x, this.y, this.size, this.size);
+  }
+
+  dir(x, y) {
+    this.xSpeed += x;
+    this.ySpeed += y;
   }
 }
